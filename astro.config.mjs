53c5b5930e84dc7eb5bd0497ce4e87b79ca7e82c
Capt. Michael <CaptMichaelDev@gmail.com>
2026-02-02 00:02:20 -0500

    feat: script of deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,10 +5,14 @@ import icon from "astro-icon";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwindcss from "@tailwindcss/vite";
+import cloudflare from "@astrojs/cloudflare";
 import { SITE } from "./src/config/site.mjs";
 
 // https://astro.build/config
 export default defineConfig({
+  adapter: cloudflare({
+    imageService: 'compile',
+  }),
   site: SITE.url,
   integrations: [
     react(),
@@ -22,6 +26,25 @@ export default defineConfig({
   ],
   vite: {
     plugins: [tailwindcss()],
+    ssr: {
+      noExternal: ["react", "react-dom"],
+    },
+    build: {
+      rollupOptions: {
+        output: {
+          banner: `
+            if (typeof MessageChannel === 'undefined') {
+              globalThis.MessageChannel = class MessageChannel {
+                constructor() {
+                  this.port1 = { onmessage: null, postMessage: (msg) => this.port2.onmessage?.({ data: msg }) };
+                  this.port2 = { onmessage: null, postMessage: (msg) => this.port1.onmessage?.({ data: msg }) };
+                }
+              };
+            }
+          `,
+        },
+      },
+    },
   },
   build: {
     inlineStylesheets: "auto",
